1a670719cbd535f48f0394bb36ba3d63f238f9b0
tanomartin05 <tanomartin05@gmail.com>
2015-03-26 15:33:20 +0000

    Agregado de PlayOff por torneo.

---
+++ presalguerofutbol/estadisticasPlayOff.php
@@ -0,0 +1,29 @@
+<?
+include_once "backEnd/admin/include/config.inc.php";
+include_once "include/templateEngine.inc.php";
+include_once "backEnd/model/goleadores.php";
+include_once "backEnd/model/torneos.php";
+include_once "backEnd/model/torneos.zonas.php";
+
+$torneosZonas = array();
+$oObj = new Torneos();
+$torneos = $oObj->getActivos();
+if ($torneos != NULL) {
+	foreach ($torneos as $torneo) {
+		$oZonas = new TorneoZona();
+		$zonas = $oZonas->getByTorneo($torneo["id"]);
+		$torneosZonas[$torneo["id"]] = array("torneo" => $torneo, "zonas" => $zonas);
+	}
+}
+
+$torneoActivo = $oObj->get($_GET['idTorneoActivo']);
+$nombreArchivo = "images/playOff".$torneoActivo[0]['nombre'].".jpg";
+
+if (!file_exists ($nombreArchivo)) {
+	$nombreArchivo = "NULL";
+}
+
+// Cargo la plantilla
+$twig->display('estadisticasPlayOff.html',array("torneosZonas" => $torneosZonas, "imagen" => $nombreArchivo, "idTorneoActivo" => $_GET['idTorneoActivo']));
+
+?>
